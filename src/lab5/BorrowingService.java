994e11672d1de86f99f357b55836dded4b2046d6
BaileyM4556 <baileymawhinney4556@gmail.com>
2024-11-22 16:05:19 -0800

    Part 5.3 Changes

--- src/lab5/BorrowingService.java
+++ src/lab5/BorrowingService.java
@@ -4,17 +4,23 @@ public class BorrowingService implements BorrowingServiceAPI{
 
 		@Override
 		public boolean borrowBook(Member member, Book book) {
-		// Here you can implement logic to check if the book is available to
-		// borrow and if the member can borrow it
-		//(e.g., item limit, member status).
-		System.out.println("Borrowing book: " + book);
-		return true; // Return true for success
+			System.out.println("Borrowing book: " + book);
+			if (book != null && book.getIsAvailable() == true && !member.getBorrowedBooks().contains(book)) {
+				member.borrowedBooks.add(book);
+				book.setIsAvailable(false);
+				return true;
+			}
+			return false;
 		}
 		@Override
 		public boolean returnBook(Member member, Book book) {
-		// Implement logic to handle returning a book
-		System.out.println("Returning book: " + book);
-		return true; // Return true for success
+			System.out.println("Returning book: " + book);
+			if (member.getBorrowedBooks().contains(book)) {
+				member.borrowedBooks.remove(book);
+				book.setIsAvailable(true);
+				return true;
+			}
+			return false;
 		}
 
 }
